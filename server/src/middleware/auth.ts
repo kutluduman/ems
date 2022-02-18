51a02253e2e3959c962b8504be5d9f9e92d77c78
kutluhan <kutluduman@gmail.com>
2022-02-18 13:29:51 -0500

    Authorization middleware is created

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,29 @@
+import { Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+import { AuthenticatedRequest } from "../interfaces/AuthenticatedRequest";
+import config from "../config";
+
+const { JWT_SECRET } = config;
+
+export default (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.Authorization;
+
+  // Check for token
+  if (!token)
+    return res.status(401).json({ msg: "No token, authorization denied" });
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, JWT_SECRET);
+    // Add employee from payload
+    req.employee = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({ msg: "Token is not valid" });
+  }
+};
