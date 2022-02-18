9cd298c0063da5c04abb093a66a35e7af8517a5d
kutluhan <kutluduman@gmail.com>
2022-02-18 14:31:29 -0500

    Mock data was created for test purpose

---
+++ server/src/controllers/__test__/auth.mock.ts
@@ -0,0 +1,38 @@
+export const FIND_BY_EMAIL_MOCK = {
+  _id: "620cd780b8f5c24b04694fd6",
+  name: "Janelia",
+  surname: "Doe",
+  phoneNumber: "0987654321",
+  address: "1 Wall St., New York City, New York",
+  title: "Developer",
+  email: "jane@gmail.com",
+  password: "$2a$10$TDQkFJutkfA0N71ppYkfgOYe6j8hglhuvc.8aMN9/c4AUV7ephWO6",
+  __v: 0,
+};
+
+export const FIND_BY_ID_MOCK = {
+  _id: "620cd780b8f5c24b04694fd6",
+  name: "Jacob",
+  surname: "Doe",
+  phoneNumber: "0987654321",
+  address: "1 Wall St., New York City, New York",
+  title: "Developer",
+  email: "jane@gmail.com",
+  password: "$2a$10$TDQkFJutkfA0N71ppYkfgOYe6j8hglhuvc.8aMN9/c4AUV7ephWO6",
+  __v: 0,
+};
+
+export const NEW_EMPLOYEE_MOCK = {
+  _id: "620cd780b8f5c24b04694fd6",
+  name: "John",
+  surname: "Doe",
+  phoneNumber: "0987654321",
+  address: "1 Wall St., New York City, New York",
+  title: "Developer",
+  email: "jane@gmail.com",
+  password: "$2a$10$TDQkFJutkfA0N71ppYkfgOYe6j8hglhuvc.8aMN9/c4AUV7ephWO6",
+  __v: 0,
+};
+
+export const TOKEN_MOCK =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMGNlZTk0YjhmNWMyNGIwNDY5NGZkYyIsImlhdCI6MTY0NTE4ODE0NCwiZXhwIjoxNjQ1MTkxNzQ0fQ.hCVJOYdHh5wq2EWr-1KF4rHLH9__LlugvFpqshCGMDw";
---
+++ server/src/controllers/__test__/employees.mock.ts
@@ -0,0 +1,75 @@
+export const FIND_MOCK = [
+  {
+    _id: "620cd780b8f5c24b04694fd6",
+    name: "Janelia",
+    surname: "Doe",
+    phoneNumber: "0987654321",
+    address: "1 Wall St., New York City, New York",
+    title: "Developer",
+    email: "jane@gmail.com",
+    password: "$2a$10$TDQkFJutkfA0N71ppYkfgOYe6j8hglhuvc.8aMN9/c4AUV7ephWO6",
+    __v: 0,
+  },
+  {
+    _id: "620e3340fdb2b250e39025f3",
+    name: "Jacob",
+    surname: "Riglin",
+    phoneNumber: "0964728219",
+    address: "10 London St. London",
+    title: "Photographer",
+    __v: 0,
+  },
+  {
+    _id: "620e46090fbbeb7702a0ec15",
+    name: "Sam",
+    surname: "Kolder",
+    phoneNumber: "0987654321",
+    address: "123, Silverwater Road, Toronto, CA",
+    title: "Photographer & Videographer",
+    __v: 0,
+  },
+  {
+    _id: "620e57fdcbb0ab0b0b0c88a6",
+    name: "John",
+    surname: "Doe",
+    phoneNumber: "0987654321",
+    address: "jsdgfhjgdfks",
+    title: "sjkdhfjk",
+    __v: 0,
+  },
+];
+
+export const FIND_BY_ID_MOCK = {
+  _id: "620cd780b8f5c24b04694fd6",
+  name: "Janelia",
+  surname: "Doe",
+  phoneNumber: "0987654321",
+  address: "1 Wall St., New York City, New York",
+  title: "Developer",
+};
+
+export const FIND_BY_ID_AND_UPDATE_INPUT_MOCK = {
+  _id: "620e57fdcbb0ab0b0b0c88a6",
+  name: "John",
+  surname: "Doe",
+  phoneNumber: "0987654321",
+  address: "jsdgfhjgdfks",
+  title: "sjkdhfjk",
+  __v: 0,
+};
+
+export const FIND_BY_ID_AND_UPDATE_OUTPUT_MOCK = {
+  _id: "620e3340fdb2b250e39025f3",
+  name: "Jacob",
+  surname: "Riglin",
+  phoneNumber: "0964728219",
+  address: "10 London St. London",
+  title: "Photographer",
+  __v: 0,
+};
+
+export const EMPLOYEES_TO_DELETE_MOCK = [
+  "620cd780b8f5c24b04694fd6",
+  "620cee94b8f5c24b04694fdc",
+  "620e3340fdb2b250e39025f3",
+];
