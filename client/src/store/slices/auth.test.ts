0aef59ad1370509f5187e295c698c6fad51f112e
kutluhan <kutluduman@gmail.com>
2022-02-19 17:12:07 -0500

    Redux store state tests

---
+++ client/src/store/slices/auth.test.ts
@@ -0,0 +1,31 @@
+import { AsyncStatus } from "../common";
+import authReducer, { AuthState, logout, toggleRememberMe } from "./auth";
+
+describe("Auth Reducer", () => {
+  const initialState: AuthState = {
+    status: AsyncStatus.IDLE,
+    rememberMe: true,
+  };
+
+  describe("sync actions", () => {
+    test("should handle initial state", () => {
+      expect(authReducer(undefined, { type: "unknown" })).toEqual({
+        status: AsyncStatus.IDLE,
+        rememberMe: true,
+        token: null,
+      });
+    });
+
+    test("should handle toggleRememberMe", () => {
+      let actual = authReducer(initialState, toggleRememberMe());
+      expect(actual.rememberMe).toBeFalsy();
+    });
+
+    test("should handle logout", () => {
+      const actual = authReducer(initialState, logout());
+      expect(actual.token).toBeFalsy();
+      expect(actual.employee).toBeFalsy();
+      expect(actual.error).toBeFalsy();
+    });
+  });
+});
