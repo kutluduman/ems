917043711e2982ddcc53d70e18b0a556fcf42c38
kutluhan <kutluduman@gmail.com>
2022-02-19 13:47:10 -0500

    Layouts are created for the pages

---
+++ client/src/layouts/dashboard/AccountPopover.tsx
@@ -0,0 +1,90 @@
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { alpha } from "@mui/material/styles";
+import {
+  Button,
+  Box,
+  Divider,
+  Typography,
+  Avatar,
+  IconButton,
+} from "@mui/material";
+
+import { logout, selectEmployee } from "../../store/slices/auth";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import MenuPopover from "../../components/MenuPopover";
+
+const AccountPopover = () => {
+  const anchorRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const account = useAppSelector(selectEmployee);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <IconButton
+        ref={anchorRef}
+        onClick={handleOpen}
+        sx={{
+          padding: 0,
+          width: 44,
+          height: 44,
+          ...(open && {
+            "&:before": {
+              zIndex: 1,
+              content: "''",
+              width: "100%",
+              height: "100%",
+              borderRadius: "50%",
+              position: "absolute",
+              bgcolor: (theme) => alpha(theme.palette.grey[900], 0.72),
+            },
+          }),
+        }}
+      >
+        <Avatar
+          src="https://minimal-kit-react.vercel.app/static/mock-images/avatars/avatar_default.jpg"
+          alt="photoURL"
+        />
+      </IconButton>
+
+      <MenuPopover
+        open={open}
+        onClose={handleClose}
+        anchorEl={anchorRef.current}
+        sx={{ width: 220 }}
+      >
+        <Box sx={{ my: 1.5, px: 2.5 }}>
+          <Typography variant="subtitle1" noWrap>
+            {account?.name} {account?.surname}
+          </Typography>
+          <Typography variant="body2" sx={{ color: "text.secondary" }} noWrap>
+            {account?.email}
+          </Typography>
+        </Box>
+
+        <Divider sx={{ my: 1 }} />
+
+        <Box sx={{ p: 2, pt: 1.5 }}>
+          <Button
+            fullWidth
+            color="inherit"
+            variant="outlined"
+            onClick={() => {
+              dispatch(logout());
+              navigate("/login", { replace: true });
+            }}
+          >
+            Logout
+          </Button>
+        </Box>
+      </MenuPopover>
+    </>
+  );
+}
+
+export default AccountPopover;
---
+++ client/src/layouts/dashboard/SidebarConfig.tsx
@@ -0,0 +1,11 @@
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+
+const sidebarConfig = [
+  {
+    title: "employees",
+    path: "/dashboard/employees",
+    icon: <PeopleAltIcon />,
+  },
+];
+
+export default sidebarConfig;
