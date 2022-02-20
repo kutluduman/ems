7953431ede7c9ffa980f8558cca6b09e7b3aa80f
kutluhan <kutluduman@gmail.com>
2022-02-20 14:02:14 -0500

    App configuration

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { render } from "react-dom";
+import { StrictMode } from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import "./index.css";
+import { store } from "./store";
+import App from "./App";
+
+render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
